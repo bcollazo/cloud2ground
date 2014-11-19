47b85af6ce6cfd3c5e1f9e40c79f6edf4c8b0620
bcollazo <bcollazo@mit.edu>
2014-11-19 10:12:03 -0500

    asdf

--- c2g_server/routes/index.js
+++ c2g_server/routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET home page. */
+router.get('/slideshows', function(req, res) {
+  res.json([
+  		{"title": "How To Start A Startup - Lec 1",
+  		"publisher": "Y-Combinator"},
+  		{"title": "Algebra 1 - Lec 1",
+  		"publisher": "Khan Academy"},
+  		{"title": "Intro to Programming",
+  		"publisher": "edX"},
+  	]);
+});
+
 module.exports = router;
